ce55ac511d16b7dc6ce52f9e426ff509f4848762
js-rushing <unoriginal9000@yahoo.com>
2020-06-14 20:02:01 -0500

    finshed intermediate algorithm scripting module

--- javaScript/intermediate_algorithm_scripting/mapTheDebris.js
+++ javaScript/intermediate_algorithm_scripting/mapTheDebris.js
@@ -9,19 +9,42 @@ document.getElementById('pageTitle').innerHTML = "Map the Debris";
 
 // The values should be rounded to the nearest whole number. The body being orbited is Earth.
 
-// The radius of the earth is 6367.4447 kilometers, and the GM value of earth is 398600.4418 km3s-2.
+// The radius of the earth is 6367.4447 kilometers, and the GM value of earth is 398600.4418 km^3s^-2.
 
 
 // My Solution
 function orbitalPeriod(arr) {
+    var newArr = [];
     var GM = 398600.4418;
     var earthRadius = 6367.4447;
-    return arr;
+    function Answer(title, period) {
+        this.name = title;
+        // this.orbitalPeriod = period;
+        this.orbitalPeriod = Math.round((2 * Math.PI) * (Math.sqrt((Math.pow((period + earthRadius), 3)) / GM)));;
+    }
+    for (let i = 0; i < arr.length; i++) {
+        let bob = new Answer(arr[i].name, arr[i].avgAlt);
+        newArr.push(bob);
+    }
+    return newArr;
 }
 
+// Their Solution
+// function orbitalPeriod(arr) {
+//     const GM = 398600.4418;
+//     const earthRadius = 6367.4447;
+//     return arr.map(({ name, avgAlt }) => {
+//         const earth = earthRadius + avgAlt;
+//         const orbitalPeriod = Math.round(2 * Math.PI * Math.sqrt(Math.pow(earth, 3) / GM));
+//         return { name, orbitalPeriod };
+//     });
+// }
+
 // Testing
-orbitalPeriod([{ name: "sputnik", avgAlt: 35873.5553 }]);
+// orbitalPeriod([{ name: "sputnik", avgAlt: 35873.5553 }]);
+console.log(orbitalPeriod([{ name: "sputnik", avgAlt: 35873.5553 }]));
 // should return [{name: "sputnik", orbitalPeriod: 86400}].
 
-orbitalPeriod([{ name: "iss", avgAlt: 413.6 }, { name: "hubble", avgAlt: 556.7 }, { name: "moon", avgAlt: 378632.553 }]);
+// orbitalPeriod([{ name: "iss", avgAlt: 413.6 }, { name: "hubble", avgAlt: 556.7 }, { name: "moon", avgAlt: 378632.553 }]);
+console.log(orbitalPeriod([{ name: "iss", avgAlt: 413.6 }, { name: "hubble", avgAlt: 556.7 }, { name: "moon", avgAlt: 378632.553 }]));
 // should return [{name : "iss", orbitalPeriod: 5557}, {name: "hubble", orbitalPeriod: 5734}, {name: "moon", orbitalPeriod: 2377399}].
